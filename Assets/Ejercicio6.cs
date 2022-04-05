a368ac66e933f9eedb060355f176217694effb76
ArielAlzogarayFlores <102029851+ArielAlzogarayFlores@users.noreply.github.com>
2022-04-05 10:36:55 -0300

    Lots of changes

---
+++ Assets/Ejercicio6.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio6 : MonoBehaviour
+{
+    public int num1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (num1 <= 0)
+        {
+            Debug.Log("El número que ha ingresado no es válido.");
+        }
+
+        else if (num1 % 2 == 0)
+        {
+            Debug.Log("El número es par.");
+        }
+
+        else
+        {
+            Debug.Log("El número es impar.");
+        }
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
