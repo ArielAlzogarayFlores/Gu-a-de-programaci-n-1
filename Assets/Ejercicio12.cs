cf92ea8ac569a12d84802d698a9b0f74bd68b353
ArielAlzogarayFlores <102029851+ArielAlzogarayFlores@users.noreply.github.com>
2022-04-06 23:16:02 -0300

    Exercise 12 finished

--- Assets/Ejercicio12.cs
+++ Assets/Ejercicio12.cs
@@ -11,8 +11,63 @@ public class Ejercicio12 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-     
-        
+        //Áca empiezán los "Excelente.".
+
+        if (dado1 == 6 && dado1 == dado2 && dado2 == dado3)
+        {
+            Debug.Log("Excelente.");
+        }
+
+        else if (dado2 == 6 && dado2 == dado1 && dado1 == dado3)
+        {
+            Debug.Log("Excelente.");
+        }
+
+        else if (dado3 == 6 && dado3 == dado2 && dado2 == dado1)
+        {
+            Debug.Log("Excelente.");
+        }
+
+        //Áca empiezán los "Muy bien.".
+
+        else if (dado1 == 6 && dado1 == dado2 && dado2 != dado3)
+        {
+            Debug.Log("Muy bien.");
+        }
+
+        else if (dado2 == 6 && dado2 == dado3 && dado3 != dado1)
+        {
+            Debug.Log("Muy bien.");
+        }
+
+        else if (dado3 == 6 && dado3 == dado1 && dado1 != dado2)
+        {
+            Debug.Log("Muy bien.");
+        }
+
+        //Áca empiezán los "Regular.".
+
+        else if (dado1 == 6 && dado1 != dado2 && dado1 != dado3)
+        {
+            Debug.Log("Regular.");
+        }
+
+        else if (dado2 == 6 && dado2 != dado1 && dado2 != dado3)
+        {
+            Debug.Log("Regular.");
+        }
+
+        else if (dado3 == 6 && dado3 != dado2 && dado3 != dado1)
+        {
+            Debug.Log("Regular.");
+        }
+
+        else
+        {
+            Debug.Log("Insuficiente.");
+        }
+
+
     }
 
     // Update is called once per frame
